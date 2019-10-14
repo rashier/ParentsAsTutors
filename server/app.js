14487b606d03d06833533a7a8b079af379ac5ebd
rashier <rashier13@gmail.com>
2019-10-14 18:14:58 +0200

    fix favicon

--- server/app.js
+++ server/app.js
@@ -52,7 +52,8 @@ require('./passport')(app);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 const authRouter = require('./routes/auth.routes');
 app.use('/', authRouter);
